03cc7addbbfc42517e4366806407df660c812172
Shinrael <mircoelena89@gmail.com>
2024-04-04 14:59:09 +0100

    [add] star solid

--- src/components/partials/Moviecard.vue
+++ src/components/partials/Moviecard.vue
@@ -18,8 +18,14 @@
       } else {
         return null;
       }
-      }
+      },
+      calculatedRate() {
+      return Math.ceil(this.rate / 2);
     }
+    },
+    methods:{
+      
+    }    
   }
 </script>
 
@@ -32,15 +38,22 @@
           <img :src="'https://image.tmdb.org/t/p/w300/' + poster_path" :alt="title">
         </div>
         <div class="flip-card-back">
-          <h1>{{ title }} {{ name }} </h1> 
-          <h3>{{ originalTitle }} {{ original_name }}</h3>
+          <h3>Titolo: {{ title }} {{ name }} </h3> 
+          <h5>Titolo Originale: {{ originalTitle }} {{ original_name }}</h5>
           <div v-if="languageFlag">
             <img :src="languageFlag" alt="Bandiera lingua">
           </div>
           <div v-else>
-            <p>{{ original_language }}</p> 
+            <p> Lingua Originale: {{ original_language }}</p> 
           </div>
-          <p>{{ rate }}</p>
+          <div class="stars">
+           <p>Voto:
+            <i
+             class="fa-solid fa-star"
+             v-for="index in calculatedRate"
+             :key="index"
+             ></i></p>
+          </div>
         </div>
       </div>
     </div>
@@ -51,8 +64,8 @@
 <style lang="scss" scoped>
   .flip-card {
   background-color: transparent;
-  width: 300px;
-  height: 450px;
+  width: 302px;
+  height: 452px;
   perspective: 1000px;
   &:hover .flip-card-inner{
     transform: rotateY(180deg);
@@ -64,6 +77,7 @@
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
+  border: 1px solid white;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   .flip-card-front, .flip-card-back {
   position: absolute;
@@ -77,7 +91,7 @@
   color: black;
   }
   .flip-card-back {
-  background-color: #2980b9;
+  background-color: black;
   color: white;
   transform: rotateY(180deg);
   img{
